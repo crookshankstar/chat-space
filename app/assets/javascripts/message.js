1f32f7e4984b62d55604bb335da9c58a4777b652
crookshankstar <n63.hoshi.kentaro@gmail.com>
2019-12-19 18:52:36 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -106,7 +106,7 @@ $('#new_message').on('submit', function(e){
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     })
     .fail(function() {
-      console.('error');
+      console.log('error');
     });
   };
 
